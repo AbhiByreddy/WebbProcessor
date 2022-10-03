045abcf9c212c7081b1fe265e49839f9fdcb3a8a
Collin <collinjbarber@gmail.com>
2022-10-03 18:39:27 -0400

    UI

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,23 +1,9 @@
-import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
-
 version = "1.0.0"
-
-buildscript {
-    repositories {
-        maven {
-            setUrl("https://plugins.gradle.org/m2/")
-        }
-    }
-    dependencies {
-        classpath("org.openjfx:javafx-plugin:0.0.13")
-    }
-}
-apply(plugin = "org.openjfx.javafxplugin")
 
 plugins {
     java
+    application
     id("org.openjfx.javafxplugin") version "0.0.13"
-    id("com.github.johnrengelman.shadow") version "7.1.0"
 }
 
 configurations {
@@ -37,20 +23,6 @@ javafx {
     modules("javafx.controls", "javafx.fxml")
 }
 
-tasks {
-    compileJava {
-        options.encoding = Charsets.UTF_8.name() // We want UTF-8 for everything
-        options.release.set(17) // We need to set release compatibility to java 17 since MC 18+ uses it
-    }
-}
-
-tasks.named<ShadowJar>("shadowJar") {
-
-    destinationDirectory.set(file("../build"))
-    archiveFileName.set("WebbProcessor-${version}.jar")
-    configurations = listOf(project.configurations["shadeOnly"], project.configurations["runtimeClasspath"])
-}
-
-tasks.named("assemble").configure {
-    dependsOn("shadowJar")
+application {
+    mainClass.set("me.cjcrafter.webb.ui.Main")
 }
