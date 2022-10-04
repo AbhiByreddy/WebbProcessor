7e58aa17dbd6a264f91943262b24db70281b6db6
Collin <collinjbarber@gmail.com>
2022-10-03 23:27:08 -0400

    example style sheet for UI

--- src/main/java/me/cjcrafter/webb/ui/Main.java
+++ src/main/java/me/cjcrafter/webb/ui/Main.java
@@ -1,10 +1,12 @@
 package me.cjcrafter.webb.ui;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+
+import java.net.URL;
 
 // https://www.youtube.com/watch?v=9XJicRt_FaI
 public class Main extends Application {
@@ -15,12 +17,22 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
+
+        // Load the FXML file, which stores the UIs layout/buttons.
+        URL interfaceUrl = getClass().getResource("/sample.fxml");
+        if (interfaceUrl == null) throw new IllegalArgumentException("Could not find UI fxml file");
+        Parent root = FXMLLoader.load(interfaceUrl);
+
+        // Load the style sheet
+        URL styleUrl = getClass().getResource("/style.css");
+        if (styleUrl == null) throw new IllegalArgumentException("Could not find style sheet");
+        String style = styleUrl.toExternalForm();
+
         primaryStage.setTitle("WebbProcessor");
 
-        StackPane layout = new StackPane();
-        layout.getChildren().add(new Button("Click me"));
+        Scene scene = new Scene(root);
+        scene.getStylesheets().add(style);
 
-        Scene scene = new Scene(layout, 300, 300);
         primaryStage.setScene(scene);
         primaryStage.show();
     }
